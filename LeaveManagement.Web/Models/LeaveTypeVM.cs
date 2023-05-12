a5d434dbf554248c1fb856bdcee1ca4c10eaa191
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-05-11 17:22:07 -0700

    Set-up Index Views with ViewModels and AutoMapper

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [DisplayName("Leave Type Name")]
+        public string Name { get; set; }
+        [DisplayName("Default Number of Days")]
+        //[Display(Name = "Default Number of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
